d2fa62c94c8a99ddaf75f3e58d356f223b0675b3
Oleksandr Tokar <oleksandr.tokar.publish@gmail.com>
2023-12-08 01:03:02 +0300

    Some refactor

--- Assets/Packages/weather-service/Runtime/Network/GetWeatherRequests/GetWeatherFromOpenWeatherRequest.cs
+++ Assets/Packages/weather-service/Runtime/Network/GetWeatherRequests/GetWeatherFromOpenWeatherRequest.cs
@@ -10,6 +10,7 @@ namespace WeatherService.Runtime.Network.GetWeatherRequests
         private const string APIKey = "1d540708e90bf61d1f03787a60292739";
 
         protected override string BaseURL => "https://api.openweathermap.org/data/2.5/weather?";
+        
         protected override string SetRequestParams(float latitude, float longitude) =>
             $"lat={latitude}&lon={longitude}&appid={APIKey}";
     }
